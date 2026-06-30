d9399b9606732148574e1958f5119cbac7115479
GTTeancum <smmelton@gmail.com>
2026-06-30 11:03:48 -0400

    Clean Xbox random mission ownership

--- code/qcommon/cm_load_xbox.cpp
+++ code/qcommon/cm_load_xbox.cpp
@@ -4,6 +4,9 @@
 #include "cm_patch.h"
 #include "../renderer/tr_local.h"
 #include "../RMG/RM_Headers.h"
+#ifdef _XBOX
+#include "../win32/xb_log.h"
+#endif
 
 #include "sparc.h"
 #include "../zlib/zlib.h"
@@ -848,7 +851,21 @@ void CM_ClearMap( void )
 //	MAT_Shutdown();
 #endif
 
-#ifndef _XBOX
+#ifdef _XBOX
+	if (TheRandomMissionManager)
+	{
+		XBLF("JA: CM_ClearMap deleting TheRandomMissionManager ptr=%p", TheRandomMissionManager);
+		delete TheRandomMissionManager;
+		TheRandomMissionManager = 0;
+	}
+
+	if (cmg.landScape)
+	{
+		XBLF("JA: CM_ClearMap deleting landscape ptr=%p", cmg.landScape);
+		delete cmg.landScape;
+		cmg.landScape = 0;
+	}
+#else
 	if (TheRandomMissionManager)
 	{
 		delete TheRandomMissionManager;
